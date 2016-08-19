3dc8533a252f0143bebaef28231c5548f3b16738
Jianru Lin <jianru.lin@gmail.com>
2016-08-19 16:17:10 +0800

    支持 html 渲染

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var Vast = require('./lib/Vast')
+var text_render = require('fmtjs-att-render-text')
+var html_render = require('fmtjs-att-render-html')
 
 function render(att, options) {
-	return Vast.toText(att, {})
+	options = fill_default_value(options)
+	switch (options.mode) {
+		case 'text':
+			return text_render(att, options)
+		case 'html':
+			return html_render(att, options)
+		default:
+			throw new Error('invaid render mode: ' + options.mode)
+	}
+
+	function fill_default_value(options) {
+		options = options || {}
+		options.mode = options.mode || 'text'
+		return options
+	}
 }
 
 module.exports = exports = render
